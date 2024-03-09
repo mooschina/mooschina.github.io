c16c9004ff5551059bfab44de3819639a1563720
cobwebs <161010882+mooschina@users.noreply.github.com>
2024-03-09 09:01:23 +0800

    js.js

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
-
+function Time()
+							{
+								x=document.getElementById("time");  // 找到元素
+								var d=new Date();
+								var y = d.getFullYear();
+								var m = d.getMonth() + 1;
+								var day = d.getDate();
+								var hour = d.getHours();
+								var min = d.getMinutes();
+								var s = d.getSeconds();
+								if (s<=9){s = "0"+s;}
+								if (min<=9){min = "0"+min;}
+								x.innerHTML=y+"年"+m+"月"+day+"日 "+hour+":"+min+":"+s;    // 改变内容
+							}
