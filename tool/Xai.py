42f25d3121ea94a89299d410a3731fbd3350d085
cobwebs <161010882+mooschina@users.noreply.github.com>
2024-04-27 20:33:57 +0800

    Xai.py

--- tool/Xai.py
+++ tool/Xai.py
@@ -1,11 +1,11 @@
 from random import *
 def event(elist,nlest):
     num = 0
-    for i in range(nlest):
+    for i in range(len(nlest)):
         num += nlest[i]
     a = randint(1,num)
     num = nlest[0]
-    for i in range(nlest):
+    for i in range(len(nlest)):
         if a<=num and a>=num-nlest[i]:
             return elist[i]
         num += nlest[i]
